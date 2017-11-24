7cb49c16e6f68f0705335bd8add7f9d113dcbba8
Igor <igor2000@inbox.ru>
2017-11-24 18:29:59 +0200

    newPackage

--- src/com/oop/lesson1/task4/UserReader.java
+++ src/com/oop/lesson1/task4/UserReader.java
@@ -3,7 +3,6 @@ package lesson1.task4;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
-import java.util.Date;
 
 /**
  * Created by MM on 24.11.2017.
@@ -11,9 +10,9 @@ import java.util.Date;
 public class UserReader {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream("Users.data"));
-        System.out.println((User) inputStream.readObject());
-        System.out.println((User) inputStream.readObject());
-        System.out.println((Date) inputStream.readObject());
+        System.out.println(inputStream.readObject());
+        System.out.println(inputStream.readObject());
+        System.out.println(inputStream.readObject());
         inputStream.close();
     }
 }
